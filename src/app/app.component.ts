d954280d589a8b1a71310ee595fb3b125339ae20
mashirojs <mashiro.js@gmail.com>
2022-04-13 11:49:07 +0900

    commit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  ngOnInit() {
+    console.log('commit');
+  }
 }
